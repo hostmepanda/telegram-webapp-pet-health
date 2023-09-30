23ed48b92d2392bf99b5a7bd877a6668b3a477f9
hostmepanda <noah.vite@gmail.com>
2023-09-30 17:55:56 +0300

    Add web_app url

--- backend/src/start-bot.ts
+++ backend/src/start-bot.ts
@@ -1,6 +1,7 @@
 import TelegramBot from 'node-telegram-bot-api';
 
 const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN || 'no-bot-token';
+const WEB_APP_URL = process.env.WEB_APP_URL || 'https://locahost:3000'
 
 const petHealthBot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: true });
 
@@ -17,7 +18,7 @@ petHealthBot.on('message', async (msg) => {
           inline_keyboard: [
             [{
               text: 'Open pet\'s diary',
-              web_app: { url: 'http://localhost:3000' }
+              web_app: { url: WEB_APP_URL }
             }],
           ],
       }
