0465d0ebe97ec736d98beff3169f71532da5570e
hostmepanda <noah.vite@gmail.com>
2023-10-01 01:07:43 +0300

    Add initData

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,12 +1,13 @@
 import './App.css'
 
-const tg = window.Telegram.webApp;
+const tg = window.Telegram?.webApp;
 
 function App() {
   return (
     <div className="App">
       <h1>Pet Health Diary!</h1>
       <h2>Welcome {tg?.initDataUnsafe?.user?.username}</h2>
+      <h3>{tg?.initData}</h3>
     </div>
   );
 }
