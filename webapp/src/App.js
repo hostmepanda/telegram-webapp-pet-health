a1741150776f933bdec2e3af5507958201ab9bdb
hostmepanda <noah.vite@gmail.com>
2023-10-01 01:09:00 +0300

    Add initData

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <h1>Pet Health Diary!</h1>
       <h2>Welcome {tg?.initDataUnsafe?.user?.username}</h2>
-      <h3>{tg?.initData}</h3>
+      <h3>{JSON.stringify(window.Telegram)}</h3>
     </div>
   );
 }
