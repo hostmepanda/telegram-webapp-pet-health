d721d2d73da98cacae1a902c41b2cbdcabc1690c
hostmepanda <noah.vite@gmail.com>
2023-09-30 18:03:02 +0300

    Add ref to tg and print username

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,9 +1,12 @@
 import './App.css'
 
+const tg = window.Telegram.webApp;
+
 function App() {
   return (
     <div className="App">
       <h1>Pet Health Diary!</h1>
+      <h2>Welcome {tg.initDataUnsafe?.user?.username}</h2>
     </div>
   );
 }
