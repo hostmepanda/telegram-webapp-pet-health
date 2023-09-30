cddc4145190c84c5545dd4d8798f3856cbfba0a5
hostmepanda <noah.vite@gmail.com>
2023-10-01 01:16:24 +0300

    Use user fullName or Nick from TG

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,13 +1,25 @@
 import './App.css'
 
-const tg = window.Telegram?.webApp;
-
 function App() {
+  const webAppData = window.Telegram.webApp;
+
+  const {
+    initDataUnsafe: {
+      user: {
+        id: userId,
+        first_name: userFirstName,
+        last_name: userLastName,
+        username,
+      } = {},
+    } = {},
+  } = webAppData
+
+  const userNameOrNick = `${userFirstName} ${userLastName}`.trim() ?? username;
+
   return (
     <div className="App">
       <h1>Pet Health Diary!</h1>
-      <h2>Welcome {tg?.initDataUnsafe?.user?.username}</h2>
-      <h3>{JSON.stringify(window.Telegram)}</h3>
+      <h2>Welcome {userNameOrNick}</h2>
     </div>
   );
 }
