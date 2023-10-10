b0a6fa23ec8140f042241a0565a40c80ecb48ed2
hostmepanda <noah.vite@gmail.com>
2023-10-11 01:08:09 +0300

    Add ssl options for Expressjs

--- backend/src/start-bot.ts
+++ backend/src/start-bot.ts
@@ -2,6 +2,10 @@ import TelegramBot from 'node-telegram-bot-api';
 import * as mongoose from "mongoose";
 import express from 'express';
 import cors from 'cors';
+import https from 'https';
+import fs from 'fs';
+
+let certificates = {};
 
 import {usersRouter} from "./router/userRouter/usersRouter";
 import {diariesRouter} from "./router/diariesRouter/diariesRouter";
@@ -10,6 +14,7 @@ const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN || 'no-bot-token';
 const WEB_APP_URL = process.env.WEB_APP_URL || 'https://locahost:3000';
 const DB_URI = process.env.DB_URI || 'http://127.0.0.1';
 const LISTEN_PORT = process.env.LISTEN_PORT ?? 4000;
+const PROTOCOL_TO_SERVE = process.env.PROTOCOL_TO_SERVE === 'https' ? 'https' : 'http';
 
 (async () => {
   const expressApp = express();
@@ -23,9 +28,21 @@ const LISTEN_PORT = process.env.LISTEN_PORT ?? 4000;
   expressApp.use('/users', usersRouter);
   expressApp.use('/diaries', diariesRouter(petHealthBot));
 
-  expressApp.listen(LISTEN_PORT, () => {
-    console.log(`expressApp app listening on port ${LISTEN_PORT}`)
-  });
+
+  if (PROTOCOL_TO_SERVE === 'https') {
+    certificates = {
+      key: fs.readFileSync('key.pem'),
+      cert: fs.readFileSync('cert.pem'),
+    };
+    https.createServer(certificates, expressApp).listen(LISTEN_PORT, () => {
+      console.log(`expressApp app listening on ${PROTOCOL_TO_SERVE} port ${LISTEN_PORT}`)
+    });
+  } else {
+    expressApp.listen(LISTEN_PORT, () => {
+      console.log(`expressApp app listening on ${PROTOCOL_TO_SERVE} port ${LISTEN_PORT}`)
+    });
+
+  }
 
   try {
     console.log(`MongoDB: connecting to database...`);
